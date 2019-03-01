70315a666edbda00a8376786725ba1a96f16e9fb
Eduardo Sanz García <esanzgar@gmail.com>
2019-03-01 15:09:55 +0000

    chore: initial setup

---
+++ src/app/containers/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "workout-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "workout";
+}
