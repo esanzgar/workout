583c9e90600920f80c5d92dc45038ad8d6f5bba3
Eduardo Sanz García <esanzgar@gmail.com>
2019-03-05 14:46:10 +0000

    feat: added app-shell

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { ServerModule } from "@angular/platform-server";
 
 import { AppModule } from "./app.module";
-import { AppComponent } from "./app.component";
+import { AppComponent } from "./containers/app.component";
 import { Routes, RouterModule } from "@angular/router";
 import { AppShellComponent } from "./app-shell/app-shell.component";
 
